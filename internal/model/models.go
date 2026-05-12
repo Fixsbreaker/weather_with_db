456d5535f2d7c75f02d6658643520ad6ad4bbb3d
Fixsbreaker <alinurkunbol@gmail.com>
2026-05-12 11:59:10 +0500

    remove DTOs from domain models to ensure clean separation

--- internal/model/models.go
+++ internal/model/models.go
@@ -12,21 +12,6 @@ type User struct {
 	DeletedAt    *time.Time `json:"deleted_at,omitempty"`
 }
 
-type RegisterRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type AuthResponse struct {
-	AccessToken string `json:"access_token"`
-}
-
 type City struct {
 	ID     int64  `json:"id"`
 	UserID int64  `json:"user_id"`
@@ -40,24 +25,4 @@ type WeatherHistory struct {
 	Temperature float64   `json:"temperature"`
 	Description string    `json:"description"`
 	RequestedAt time.Time `json:"requested_at"`
-}
-
-// DTO for weather history response
-type WeatherHistoryResponse struct {
-	UserID  int64          `json:"user_id"`
-	City    string         `json:"city"`
-	History []WeatherEntry `json:"history"`
-}
-
-type WeatherEntry struct {
-	Temperature float64   `json:"temperature"`
-	Description string    `json:"description"`
-	RequestedAt time.Time `json:"requested_at"`
-}
-
-// DTO for current weather (aggregated response)
-type CityWeather struct {
-	City        string  `json:"city"`
-	Temperature float64 `json:"temperature"`
-	Description string  `json:"description"`
 }
